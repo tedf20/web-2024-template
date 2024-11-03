bd292bbd47dbc2dde57f4f7d8c4ec2f8304bd0e7
vasinikov <va.sinikov@gmail.com>
2024-11-03 15:03:03 +0000

    Make it deployable 2

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+# Exit on any error
+set -e
+
+# Ensure we're using the right Node version
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm use 16
+
+# Pull latest changes from git
+git pull origin main
+
 # Build the React app
 npm install
 npm run build
+
+# Create directory if it doesn't exist
+sudo mkdir -p /var/www/your-app
 
 # Copy build files to apache directory
 sudo rm -rf /var/www/your-app/*
