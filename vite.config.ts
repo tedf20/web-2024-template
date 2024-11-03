a917aa490f20827182137a3ed2e9ddad6ab72099
vasinikov <va.sinikov@gmail.com>
2024-11-03 17:31:19 +0000

    Make it load correctly

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/web-2024-template/",
+  base: "/",
   resolve: {
     alias: {
       'pdfjs-dist': 'pdfjs-dist/build/pdf',
